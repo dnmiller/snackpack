0a521e2dd378672920ebcff2f79d076248ae8b8d
Dan Miller <dnmiller@gmail.com>
2014-03-10 17:58:16 -0700

    More tweaks to error macros

--- include/snackpack/error.h
+++ include/snackpack/error.h
@@ -3,22 +3,7 @@
 #include "snackpack/snackpack.h"
 
 
-#ifndef SP_PRINTF
-#ifdef SP_NO_PRINT
-#define SP_PRINTF(...)
-#else
-#define SP_PRINTF(...) printf(__VA_ARGS__)
-#endif
-#endif
-
-
-#ifndef SP_FAIL
-#ifdef SP_FAIL_HARD
-#define SP_FAIL() exit(-1);
-#else
-#define SP_FAIL() do { goto fail; } while (0)
-#endif
-#endif
+/* TODO document */
 
 
 typedef enum {
@@ -36,6 +21,24 @@ typedef enum {
 extern const char * SP_ERROR_DESCR[NUM_SP_ERROR];
 
 
+#ifndef SP_PRINTF
+#ifdef SP_NO_PRINT
+#define SP_PRINTF(...)
+#else
+#define SP_PRINTF(...) printf(__VA_ARGS__)
+#endif
+#endif
+
+
+#ifndef SP_FAIL
+#ifdef SP_FAIL_HARD
+#define SP_FAIL() exit(-1);
+#else
+#define SP_FAIL() { goto fail; }
+#endif
+#endif
+
+
 #ifndef SP_PRINT_ERROR
 #define SP_PRINT_ERROR(errno) \
 { \
@@ -50,6 +53,9 @@ extern const char * SP_ERROR_DESCR[NUM_SP_ERROR];
 
 
 #ifndef SP_ASSERT_CONDITION
+#ifdef SP_NO_ASSERT
+#define SP_ASSERT_CONDITION(cond, errno)
+#else
 #define SP_ASSERT_CONDITION(cond, errno) \
 { \
     if (!(cond)) { \
@@ -58,20 +64,25 @@ extern const char * SP_ERROR_DESCR[NUM_SP_ERROR];
     } \
 }
 #endif
+#endif
 
 
+#ifndef SP_ASSERT_VALID_DIM
 #define SP_ASSERT_VALID_DIM(n) \
-    { \
+{ \
     SP_ASSERT_CONDITION((n) > 0, SP_ERROR_INVALID_DIM); \
     SP_ASSERT_CONDITION((n) <= SP_MAX_DIMENSION, SP_ERROR_DIM_TOO_LARGE); \
-    }
+}
+#endif
 
 
+#ifndef SP_ASSERT_VALID_INC
 #define SP_ASSERT_VALID_INC(n) \
-    do { \
+{ \
     SP_ASSERT_CONDITION((n) != 0, SP_ERROR_INVALID_INC); \
     SP_ASSERT_CONDITION((n) <= SP_MAX_DIMENSION, SP_ERROR_DIM_TOO_LARGE); \
-    } while (0)
+}
+#endif
 
 
 #endif
