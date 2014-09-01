8698535c7cb46014faeceaeab964d8c8c6fb3117
Dan Miller <dnmiller@gmail.com>
2014-09-01 00:18:57 -0700

    Cleanup

--- interface/python/snackpack/libloader.py
+++ interface/python/snackpack/libloader.py
@@ -11,24 +11,24 @@ class LibInterface(object):
         with open(headername, 'r') as header_file:
             header = ''.join(l for l in header_file if not l.startswith('#'))
 
-        # Load the DLL via CFFI
+        # Load the DLL via CFFI.
         ffi = FFI()
         ffi.cdef(header)
         self._dll = ffi.dlopen(libname)
 
-        # Scan for a list of functions that begin with fun_prefix
+        # Scan for a list of functions that begin with func_prefix.
         if func_prefix is None:
             func_prefix = ''
 
         # This is a hack into the CFFI implementation and may break in future
         # CFFI versions.
-        funcs = [f[len('function '):]
-                 for f in ffi._parser._declarations.keys()
-                 if f.startswith('function ' + func_prefix)]
+        funcs = [
+            f[len('function '):] for f in ffi._parser._declarations.keys()
+            if f.startswith('function ' + func_prefix)]
 
+        # Bind the functions from _dll to self.
         for func in funcs:
             attr_name = func
             if strip_prefix:
                 attr_name = attr_name[len(func_prefix):]
-                print(attr_name)
             setattr(self, attr_name, getattr(self._dll, func))
